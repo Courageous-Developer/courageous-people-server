0cd05ac19842362dbe074fdaab80612009e24f13
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-09-01 18:39:08 +0900

    Resolve conflicts
    Resolve conflicts

--- server/board/urls.py
+++ server/board/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('store', views.StoreList.as_view(), name='store_list'),
-    path('store/<int:pk>', views.StoreDetail.as_view(), name='store_detail')
+    path('store/<int:pk>', views.StoreDetail.as_view(), name='store_detail'),
+    path('review/<int:pk>', views.ReviewList.as_view(), name='review_list'),
 
 ]
+
