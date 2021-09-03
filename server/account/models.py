0896c6d19d0fe65ae5f124f826cbf7e969ae71d7
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-09-03 19:08:02 +0900

    Add Custom User Model
    Add Custom User Model

--- server/account/models.py
+++ server/account/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password):
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    UserType = (
+        (1, "User"),
+        (2, "Manager"),
+    )
+    type = models.IntegerField(db_column='user_type', choices=UserType, default=1)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
--- server/account/views.py
+++ server/account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.views import APIView
+from .models import *
+from rest_framework.parsers import JSONParser
+from rest_framework import exceptions
+from django.views.decorators.csrf import csrf_exempt
