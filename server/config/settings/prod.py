6763fe3afbbba1e616e36911d768891306f9d003
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-11-26 10:41:28 +0900

    Update prod.py

--- server/config/settings/prod.py
+++ server/config/settings/prod.py
@@ -3,4 +3,4 @@ from.base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["13.209.14.10"]
+ALLOWED_HOSTS = ["13.209.14.10", "ec2-13-209-14-10.ap-northeast-2.compute.amazonaws.com"]
